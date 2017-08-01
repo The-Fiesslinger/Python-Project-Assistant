878dd55de4db1158fdaaf81069d8591fb3372b07
ziegler-kevin <zelk@viessmann-hausautomation.com>
2017-08-01 16:16:31 +0200

    Merge branch 'master' of https://github.com/nexxdev/Python-Project-Assistant
    # Conflicts:
    #	Python-Project-Assistant/createProject.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from distutils.core import setup
 
 setup( name='Test_Mo_24.07.17',
-	   version='0.1',
+	   version='0.5',
 	   description='Test Projekt erstellen',
 	   url='https://nexxdev.github.io/Python-Project-Assistant/',
 	   author='Kevin Ziegler',
-	   author_email='example.test.com',
+	   author_email='ziegler.kevin@outlook.de',
 	   license='gpl', )
