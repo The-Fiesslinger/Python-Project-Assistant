a0daa9ab42c8b6ae346328087fb7eca1e7d25149
ziegler-kevin <zelk@viessmann-hausautomation.com>
2017-08-01 16:14:59 +0200

    Edit any file but CreateProjekt.py not working

--- Python-Project-Assistant/create.py
+++ Python-Project-Assistant/create.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-#======================================================================================================================
+# -*- coding: utf-8 -*-
+# ======================================================================================================================
 #   Python Project Assistant
 #   Copyright (C) 2017  Kevin Ziegler <ziegler.kevin@outlook.de>
 #
@@ -13,86 +14,93 @@
 #
 #   You should have received a copy of the GNU General Public License along with this program;
 #   if not, see <http://www.gnu.org/licenses/>.
-#======================================================================================================================
+# ======================================================================================================================
 
 import os
 import shutil
 
 
 class ProjectDIR:
-    def __init__(self, script_name=" ", version=" ", description=" ", url=" ", author=" ", author_email=" ", path=" ", licenses=" "):
-        self.script_name = script_name
-        self.version = version
-        self.description = description
-        self.url = url
-        self.author = author
-        self.author_email = author_email
-        self.path = path
-        self.licenses = licenses
+	def __init__( self, script_name=" ", version=" ", description=" ", url=" ", author=" ", author_email=" ", path=" ",
+				  licenses=" " ):
+		self.script_name = script_name
+		self.version = version
+		self.description = description
+		self.url = url
+		self.author = author
+		self.author_email = author_email
+		self.path = path
+		self.licenses = licenses
 
-    def create_dir(self):
-        print('Create a new project ')
-        self.script_name = input('Project Name: ')
-        self.version = input('Version: ')
-        self.description = input('Description: ')
-        self.url = input('URL: ')
-        self.author = input('Author: ')
-        self.author_email = input('Author Email: ')
-        self.path =("./" + self.script_name)
-        self.licenses = input('Lizenz: ')
-        os.makedirs(self.path)
+	def create_dir( self ):
+		print( 'Create a new project ' )
+		self.script_name = format( str( input( 'Project Name: ' ) ) )
+		try:
+			self.version = format( float( input( 'Version: ' ) ) )
+		except TypeError:
+			print( 'Bitte gültige Zahl angeben! z.B (1.0)' )
+			self.create_dir( )
+		self.description = input( 'Description: ' )
+		self.url = input( 'URL: ' )
+		self.author = input( 'Author: ' )
+		self.author_email = input( 'Author Email: ' )
+		self.path = ("./../../" + self.script_name)
+		self.licenses = input( 'Lizenz: ' )
+		os.makedirs( self.path )
 
-        os.chdir(self.path)
-        # Setup file
-        setup = open("setup.py", 'w')
-        setup.write("#!/usr/bin/env python")
-        setup.write("\n")
-        setup.write("\n")
-        setup.write("from distutils.core import setup")
-        setup.write("\n")
-        setup.write("\n")
-        setup.write("setup(name='" + self.script_name + "',\n\t"
-                    "version='" + self.version + "',\n\t"
-                    "description='" + self.description + "',\n\t"
-                    "url='" + self.url + "',\n\t"
-                    "author='" + self.author + "',\n\t"
-                    "author_email='" + self.author_email + "',\n\t"
-                    "license='" + self.licenses +"',)")
-        setup.close()
-        readme = open("README.rst", "w")
-        readme = open("LICENSE", "w")
+		os.chdir( self.path )
+		# Setup file
+		setup = open( "setup.py", 'w' )
+		setup.write( "#!/usr/bin/env python" )
+		setup.write( "\n" )
+		setup.write( "\n" )
+		setup.write( "from distutils.core import setup" )
+		setup.write( "\n" )
+		setup.write( "\n" )
+		setup.write( "setup(name='" + self.script_name + "',\n\t"
+														 "version='" + self.version + "',\n\t"
+																					  "description='" + self.description + "',\n\t"
+																														   "url='" + self.url + "',\n\t"
+																																				"author='" + self.author + "',\n\t"
+																																										   "author_email='" + self.author_email + "',\n\t"
+																																																				  "license='" + self.licenses + "',)" )
+		setup.close( )
+		readme = open( "README.rst", "w" )
+		readme = open( "LICENSE", "w" )
 
-        if self.licenses == "gpl":
-            shutil.copy('../GPLv3', './LICENSE' )
-        else:
-            exit()
+		print( self.path )
+		if self.licenses == "gpl":
+			shutil.copy( '../Python-Project-Assistant/Python-Project-Assistant/GPLv3', '../LICENSE' )
+		else:
+			exit( )
 
-        os.makedirs("./bin/" + self.script_name.lower())
+		os.makedirs( "./bin/" + self.script_name.lower( ) )
 
-        os.makedirs("./" + self.script_name.lower()+ "/test/")
-        os.chdir("./"+ self.script_name.lower() +"/")
-        init = open("__init__.py", "w")
-        mainfile = open("main.py", "w")
+		os.makedirs( "./" + self.script_name.lower( ) + "/test/" )
+		os.chdir( "./" + self.script_name.lower( ) + "/" )
+		init = open( "__init__.py", "w" )
+		mainfile = open( "main.py", "w" )
 
-        os.chdir("./test")
-        test_init = open("__init__.py", "w")
-        test_main = open("test_main.py", "w")
+		os.chdir( "./test" )
+		test_init = open( "__init__.py", "w" )
+		test_main = open( "test_main.py", "w" )
 
-    def create_class(self):
-        class_name = input("Klassen Name")
-        os.chdir("../")
-        #print(os.path.abspath("."))
-        test = open("main.py", "w")
-        test.write("#!/usr/bin/env python")
-        test.write("\n \n")
-        test.write("class " + class_name + ":\n\tpass")
-        test.close()
+	def create_class( self ):
+		class_name = input( "Klassen Name" )
+		os.chdir( "../" )
+		# print(os.path.abspath("."))
+		test = open( "main.py", "w" )
+		test.write( "#!/usr/bin/env python" )
+		test.write( "\n \n" )
+		test.write( "class " + class_name + ":\n\tpass" )
+		test.close( )
 
 
-def main():
-    create = ProjectDIR()
-    create.create_dir()
-    create.create_class()
+def main( ):
+	create = ProjectDIR( )
+	create.create_dir( )
+	create.create_class( )
+
 
 if __name__ == "__main__":
-    main()
+	main( )
--- Python-Project-Assistant/createProject.py
+++ Python-Project-Assistant/createProject.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
-# -*- coding: iso-8859-15 -*-
-#======================================================================================================================
+#!/usr/bin/env python3
+
+# ======================================================================================================================
 #   Python Project Assistant
 #   Copyright (C) 2017  Kevin Ziegler <ziegler.kevin@outlook.de>
 #
@@ -14,137 +14,135 @@
 #
 #   You should have received a copy of the GNU General Public License along with this program;
 #   if not, see <http://www.gnu.org/licenses/>.
-#======================================================================================================================
+# ======================================================================================================================
 
 import sys, os, shutil
 
-
-print('Usage: python parameter.py [...] [...] [...] ....|[crDir, crClass, crRelease, crInstall]!')
-
-class param:
-    def __init__( self, script_name=" ", version=" ", description=" ", url=" ", author=" ", author_email=" ", path=" ",
-                  licenses=" " ):
-        self.script_name = script_name
-        self.version = version
-        self.description = description
-        self.url = url
-        self.author = author
-        self.author_email = author_email
-        self.path = path
-        self.licenses = licenses
-
-    def create_dir( self ):
-        print( 'Create a new project ' )
-        self.script_name = input( 'Project Name: ' )
-        self.version = input( 'Version: ' )
-        self.description = input( 'Description: ' )
-        self.url = input( 'URL: ' )
-        self.author = input( 'Author: ' )
-        self.author_email = input( 'Author Email: ' )
-        self.path = ("./" + self.script_name)
-        self.licenses = input( 'Lizenz: ' )
-        os.makedirs( self.path )
-
-        os.chdir( self.path )
-        # Setup file
-        setup = open( "setup.py", 'w' )
-        setup.write( "#!/usr/bin/env python" )
-        setup.write( "\n" )
-        setup.write( "\n" )
-        setup.write( "from distutils.core import setup" )
-        setup.write( "\n" )
-        setup.write( "\n" )
-        setup.write( "setup(name='" + self.script_name + "',\n\t"
-                                                         "version='" + self.version + "',\n\t"
-                                                                                      "description='" + self.description + "',\n\t"
-                                                                                                                           "url='" + self.url + "',\n\t"
-                                                                                                                                                "author='" + self.author + "',\n\t"
-                                                                                                                                                                           "author_email='" + self.author_email + "',\n\t"
-                                                                                                                                                                                                                  "license='" + self.licenses + "',)" )
-        setup.close( )
-        readme = open( "README.rst", "w" )
-        lizenz = open( "LICENSE", "w" )
-
-        if self.licenses == "gpl":
-            shutil.copy( '../GPLv3', './LICENSE' )
-        else:
-            exit( )
-
-        os.makedirs( "./bin/" + self.script_name.lower( ) )
-
-        os.makedirs( "./" + self.script_name.lower( ) + "/test/" )
-        os.chdir( "./" + self.script_name.lower( ) + "/" )
-        init = open( "__init__.py", "w" )
-        mainfile = open( "main.py", "w" )
-
-        os.chdir( "./test" )
-        test_init = open( "__init__.py", "w" )
-        test_main = open( "test_main.py", "w" )
-
-    def create_class( self ):
-        class_name = input( "Klassen Name" )
-        os.chdir( "../" )
-        # print(os.path.abspath("."))
-        test = open( "main.py", "w" )
-        test.write( "#!/usr/bin/env python" )
-        test.write( "\n \n" )
-        test.write( "class " + class_name + ":\n\tpass" )
-        test.close( )
+print( 'Usage: python parameter.py [...] [...] [...] ....|[crDir, crClass, crRelease, crInstall]!' )
 
 
-    def abfrage(self):
-        try:
-            if CRDIR == 'crDir':
-                self.create_dir( )
-                print( 'Grundstrucktur wurde angelegt!')
-            else:
-                print('Grundstrucktur wurde NICHT angelegt!')
+class Param:
+	def __init__( self, script_name=' ', version=' ', description=' ', url=' ', author=' ', author_email=' ',
+				  licenses=' ' ):
+		self._script_name = script_name
+		self._version = version
+		self._description = description
+		self._url = url
+		self._author = author
+		self._author_email = author_email
+		self._licenses = licenses
 
-            if CRCLASS == 'crClass':
-                print('Klasse wurde angelegt')
-            else:
-                print('Klasse wurde NICHT angelegt!')
+	def create_dir( self ):
+		print( 'Create a new project ' )
 
-            if CRRELEASE == 'crRelease':
-                print('Release wurde angelegt')
-            else:
-                print('Release wurde NICHT angelegt!')
+		self.script_name = input( 'Project Name: ' )
+		self.version = input( 'Version: ' )
+		self.description = input( 'Description: ' )
+		self.url = input( 'URL: ' )
+		self.author = input( 'Author: ' )
+		self.author_email = input( 'Author Email: ' )
+		self.path = ("./" + self.script_name)
+		self.licenses = input( 'Lizenz: ' )
+		os.makedirs( self.path )
 
-            if CRINSTALL == 'crInstall':
-                print('Installer wurde angelegt')
-            else:
-                print('Installer wurde NICHT angelegt!')
-        except ValueError:
-            print('Fehler')
+		os.chdir( self.path )
+		# Setup file
+		setup = open( "setup.py", 'w' )
+		setup.write( "#!/usr/bin/env python" )
+		setup.write( "\n" )
+		setup.write( "\n" )
+		setup.write( "from distutils.core import setup" )
+		setup.write( "\n" )
+		setup.write( "\n" )
+		setup.write( "setup(name='" + self.script_name + "',\n\t"
+                                "version='" + self.version + "',\n\t"
+                                "description='" + self.description + "',\n\t"
+                                "url='" + self.url + "',\n\t"
+                                "author='" + self.author + "',\n\t"
+                                "author_email='" + self.author_email + "',\n\t"
+                                "license='" + self.licenses + "',)" )
+		setup.close( )
+		readme = open( "README.rst", "w" )
+		lizenz = open( "LICENSE", "w" )
+
+		if self.licenses == "gpl":
+			shutil.copy( '../GPLv3', './LICENSE' )
+		else:
+			exit( )
+
+		os.makedirs( "./bin/" + self.script_name.lower( ) )
+
+		os.makedirs( "./" + self.script_name.lower( ) + "/test/" )
+		os.chdir( "./" + self.script_name.lower( ) + "/" )
+		init = open( "__init__.py", "w" )
+		mainfile = open( "main.py", "w" )
+
+		os.chdir( "./test" )
+		test_init = open( "__init__.py", "w" )
+		test_main = open( "test_main.py", "w" )
+
+	def create_class( self ):
+		class_name = input( "Klassen Name" )
+		os.chdir( "../" )
+		# print(os.path.abspath("."))
+		test = open( "main.py", "w" )
+		test.write( "#!/usr/bin/env python" )
+		test.write( "\n \n" )
+		test.write( "class " + class_name + ":\n\tpass" )
+		test.close( )
+
+	def abfrage( self ):
+		try:
+			if CRDIR == 'crDir':
+				self.create_dir( )
+				print( 'Grundstrucktur wurde angelegt!' )
+			else:
+				print( 'Grundstrucktur wurde NICHT angelegt!' )
+
+			if CRCLASS == 'crClass':
+				print( 'Klasse wurde angelegt' )
+			else:
+				print( 'Klasse wurde NICHT angelegt!' )
+
+			if CRRELEASE == 'crRelease':
+				print( 'Release wurde angelegt' )
+			else:
+				print( 'Release wurde NICHT angelegt!' )
+
+			if CRINSTALL == 'crInstall':
+				print( 'Installer wurde angelegt' )
+			else:
+				print( 'Installer wurde NICHT angelegt!' )
+		except ValueError:
+			print( 'Fehler' )
+
 
 try:
-    CRDIR = str(sys.argv[1])
+	CRDIR = str( sys.argv[ 1 ] )
 except IndexError:
-    CRDIR = None
+	CRDIR = None
 
 try:
-    CRCLASS = str(sys.argv[2])
+	CRCLASS = str( sys.argv[ 2 ] )
 except IndexError:
-    CRCLASS = None
+	CRCLASS = None
 
 try:
-    CRRELEASE = str(sys.argv[3])
+	CRRELEASE = str( sys.argv[ 3 ] )
 except IndexError:
-    CRRELEASE = None
+	CRRELEASE = None
 
 try:
-    CRINSTALL = str(sys.argv[4])
+	CRINSTALL = str( sys.argv[ 4 ] )
 except IndexError:
-    CRINSTALL = None
+	CRINSTALL = None
 
 
+def main( ):
+	create = Param( )
+	create.__init__()
+	create.create_dir()
 
-def main():
-    create = param()
-    create.abfrage()
-    create.create_dir( )
-    create.create_class()
 
 if __name__ == "__main__":
-    main()
-
+	main( )
